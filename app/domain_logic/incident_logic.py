17f5729f06e226c6df56ff8e89d074261021a1cd
Nastya Andreeva <nastyaandreevaru@gmail.com>
2023-01-22 15:59:58 +0300

    Working incident logic draft with tests

--- app/domain_logic/incident_logic.py
+++ app/domain_logic/incident_logic.py
@@ -4,8 +4,8 @@ from app.data_structures.incident import Incident
 import app.persistence.incidents as incident_persistence
 
 
-def list_incidents(incident_id, started_at, ended_at) -> list[Incident]:
-    return incident_persistence.list_incidents(incident_id, started_at, ended_at)
+def list_incidents(from_incident_id, limit) -> list[Incident]:
+    return incident_persistence.list_incidents(from_incident_id, limit)
 
 
 def get_info_about_incident(incident_id) -> Incident:
@@ -16,8 +16,12 @@ def get_incident(incident_id) -> Incident:
     return incident_persistence.get_incident(incident_id)
 
 
-def create_incident(incident_name: str, user_id: str, started_at: datetime):
-    return incident_persistence.create_incident(incident_name, user_id, started_at)
+def create_incident(incident: Incident):
+    return incident_persistence.create_incident(incident)
+
+
+def update_incident(incident_id: str, attrs):
+    return incident_persistence.update_incident(incident_id, attrs)
 
 
 def mark_incident_as_recalled(incident_id, user_id: str) -> Incident:
@@ -28,13 +32,17 @@ def mark_incident_as_recalled(incident_id, user_id: str) -> Incident:
         raise Exception("User can not recall incident")
 
 
-def plus_one_meter_report(positive_reports_count: int, negative_reports_count: int):
+def increment_all_reports_count(positive_reports_count: int, negative_reports_count: int):
     return incident_persistence.increment_all_reports_count(negative_reports_count, positive_reports_count)
 
 
-def plus_one_meter_confirmed(positive_reports_count: int):
+def increment_positive_reports_count(positive_reports_count: int):
     return incident_persistence.increment_positive_reports_count(positive_reports_count)
 
 
-def plus_one_meter_not_confirmed(negative_reports_count: int):
+def increment_negative_reports_count(negative_reports_count: int):
     return incident_persistence.increment_negative_reports_count(negative_reports_count)
+
+
+def increment_resolved_notifications_count(incident_id):
+    return incident_persistence.increment_resolved_notifications_count(incident_id)
