6b4ee6e6878a2104da4f49181ce87e101af1664e
Nastya Andreeva <nastyaandreevaru@gmail.com>
2023-01-29 16:15:57 +0300

    Fixed functions

--- app/domain_logic/incident_logic.py
+++ app/domain_logic/incident_logic.py
@@ -8,10 +8,6 @@ def list_incidents(from_incident_id: str, limit: int) -> list[Incident]:
     return incident_persistence.list_incidents(from_incident_id, limit)
 
 
-def get_info_about_incident(incident_id: str) -> Incident:
-    return incident_persistence.info_about_incident(incident_id)
-
-
 def get_incident(incident_id: str) -> Incident:
     return incident_persistence.get_incident(incident_id)
 
@@ -32,8 +28,12 @@ def mark_incident_as_recalled(incident_id: str, user_id: str) -> Incident:
         raise Exception("User can not recall incident")
 
 
-def increment_all_reports_count(positive_reports_count: int, negative_reports_count: int):
-    return incident_persistence.increment_all_reports_count(negative_reports_count, positive_reports_count)
+def mark_incident_as_ended(incident_id: str, user_id: str) -> Incident:
+    incident = incident.get_incident(incident_id)
+    if incident.user_id == user_id:
+        return incident_persistence.update_incident(incident_id, {'ended_at': datetime.now()})
+    else:
+        raise Exception("User can not end incident")
 
 
 def increment_positive_reports_count(positive_reports_count: int):
