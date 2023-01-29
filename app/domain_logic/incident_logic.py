904cef9fb25b39d858f6f22ef1532b1a73a9b82b
Nastya Andreeva <nastyaandreevaru@gmail.com>
2023-01-29 15:27:02 +0300

    Fixed types

--- app/domain_logic/incident_logic.py
+++ app/domain_logic/incident_logic.py
@@ -4,15 +4,15 @@ from app.data_structures.incident import Incident
 import app.persistence.incidents as incident_persistence
 
 
-def list_incidents(from_incident_id, limit) -> list[Incident]:
+def list_incidents(from_incident_id: str, limit: int) -> list[Incident]:
     return incident_persistence.list_incidents(from_incident_id, limit)
 
 
-def get_info_about_incident(incident_id) -> Incident:
+def get_info_about_incident(incident_id: str) -> Incident:
     return incident_persistence.info_about_incident(incident_id)
 
 
-def get_incident(incident_id) -> Incident:
+def get_incident(incident_id: str) -> Incident:
     return incident_persistence.get_incident(incident_id)
 
 
@@ -24,7 +24,7 @@ def update_incident(incident_id: str, attrs):
     return incident_persistence.update_incident(incident_id, attrs)
 
 
-def mark_incident_as_recalled(incident_id, user_id: str) -> Incident:
+def mark_incident_as_recalled(incident_id: str, user_id: str) -> Incident:
     incident = incident.get_incident(incident_id)
     if incident.user_id == user_id:
         return incident_persistence.update_incident(incident_id, {'recalled': True})
@@ -44,5 +44,5 @@ def increment_negative_reports_count(negative_reports_count: int):
     return incident_persistence.increment_negative_reports_count(negative_reports_count)
 
 
-def increment_resolved_notifications_count(incident_id):
+def increment_resolved_notifications_count(incident_id: str):
     return incident_persistence.increment_resolved_notifications_count(incident_id)
